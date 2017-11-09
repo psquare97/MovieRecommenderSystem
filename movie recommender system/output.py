19c7ebd099878a777871abfe5bde4d54e8a1147f
Parth Rathod <17307743+psquare97@users.noreply.github.com>
2017-11-09 19:23:02 +0530

    Add files via upload

---
+++ movie recommender system/output.py
@@ -0,0 +1,126 @@
+# coding: utf-8
+    user_id  movieId  rating   timestamp  \
+0         1       31     2.5  1260759144   
+1         1     1029     3.0  1260759179   
+2         1     1061     3.0  1260759182   
+3         1     1129     2.0  1260759185   
+4         1     1172     4.0  1260759205   
+5         1     1263     2.0  1260759151   
+6         1     1287     2.0  1260759187   
+7         1     1293     2.0  1260759148   
+8         1     1339     3.5  1260759125   
+9         1     1343     2.0  1260759131   
+10        1     1371     2.5  1260759135   
+11        1     1405     1.0  1260759203   
+12        1     1953     4.0  1260759191   
+13        1     2105     4.0  1260759139   
+14        1     2150     3.0  1260759194   
+15        1     2193     2.0  1260759198   
+16        1     2294     2.0  1260759108   
+17        1     2455     2.5  1260759113   
+18        1     2968     1.0  1260759200   
+19        1     3671     3.0  1260759117   
+20        2       10     4.0   835355493   
+21        2       17     5.0   835355681   
+22        2       39     5.0   835355604   
+23        2       47     4.0   835355552   
+24        2       50     4.0   835355586   
+25        2       52     3.0   835356031   
+26        2       62     3.0   835355749   
+27        2      110     4.0   835355532   
+28        2      144     3.0   835356016   
+29        2      150     5.0   835355395   
+30        2      153     4.0   835355441   
+31        2      161     3.0   835355493   
+32        2      165     3.0   835355441   
+33        2      168     3.0   835355710   
+34        2      185     3.0   835355511   
+35        2      186     3.0   835355664   
+36        2      208     3.0   835355511   
+37        2      222     5.0   835355840   
+38        2      223     1.0   835355749   
+39        2      225     3.0   835355552   
+
+                                             title  \
+0                           Dangerous Minds (1995)   
+1                                     Dumbo (1941)   
+2                                  Sleepers (1996)   
+3                      Escape from New York (1981)   
+4   Cinema Paradiso (Nuovo cinema Paradiso) (1989)   
+5                          Deer Hunter, The (1978)   
+6                                   Ben-Hur (1959)   
+7                                    Gandhi (1982)   
+8           Dracula (Bram Stoker's Dracula) (1992)   
+9                                 Cape Fear (1991)   
+10            Star Trek: The Motion Picture (1979)   
+11          Beavis and Butt-Head Do America (1996)   
+12                   French Connection, The (1971)   
+13                                     Tron (1982)   
+14                  Gods Must Be Crazy, The (1980)   
+15                                   Willow (1988)   
+16                                     Antz (1998)   
+17                                 Fly, The (1986)   
+18                             Time Bandits (1981)   
+19                          Blazing Saddles (1974)   
+20                                GoldenEye (1995)   
+21                    Sense and Sensibility (1995)   
+22                                 Clueless (1995)   
+23                     Seven (a.k.a. Se7en) (1995)   
+24                      Usual Suspects, The (1995)   
+25                         Mighty Aphrodite (1995)   
+26                       Mr. Holland's Opus (1995)   
+27                               Braveheart (1995)   
+28                   Brothers McMullen, The (1995)   
+29                                Apollo 13 (1995)   
+30                           Batman Forever (1995)   
+31                             Crimson Tide (1995)   
+32               Die Hard: With a Vengeance (1995)   
+33                             First Knight (1995)   
+34                                 Net, The (1995)   
+35                              Nine Months (1995)   
+36                               Waterworld (1995)   
+37                        Circle of Friends (1995)   
+38                                   Clerks (1994)   
+39                               Disclosure (1994)   
+
+                                         genres  
+0                                         Drama  
+1              Animation|Children|Drama|Musical  
+2                                      Thriller  
+3              Action|Adventure|Sci-Fi|Thriller  
+4                                         Drama  
+5                                     Drama|War  
+6                        Action|Adventure|Drama  
+7                                         Drama  
+8               Fantasy|Horror|Romance|Thriller  
+9                                      Thriller  
+10                             Adventure|Sci-Fi  
+11             Adventure|Animation|Comedy|Crime  
+12                        Action|Crime|Thriller  
+13                      Action|Adventure|Sci-Fi  
+14                             Adventure|Comedy  
+15                     Action|Adventure|Fantasy  
+16  Adventure|Animation|Children|Comedy|Fantasy  
+17                 Drama|Horror|Sci-Fi|Thriller  
+18              Adventure|Comedy|Fantasy|Sci-Fi  
+19                               Comedy|Western  
+20                    Action|Adventure|Thriller  
+21                                Drama|Romance  
+22                               Comedy|Romance  
+23                             Mystery|Thriller  
+24                       Crime|Mystery|Thriller  
+25                         Comedy|Drama|Romance  
+26                                        Drama  
+27                             Action|Drama|War  
+28                                       Comedy  
+29                         Adventure|Drama|IMAX  
+30                Action|Adventure|Comedy|Crime  
+31                           Drama|Thriller|War  
+32                        Action|Crime|Thriller  
+33                         Action|Drama|Romance  
+34                        Action|Crime|Thriller  
+35                               Comedy|Romance  
+36                      Action|Adventure|Sci-Fi  
+37                                Drama|Romance  
+38                                       Comedy  
+39                               Drama|Thriller  
